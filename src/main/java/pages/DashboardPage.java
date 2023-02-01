6a095f0f21944553e730dcb142966def59e915de
vsslay <107613720+vsslay@users.noreply.github.com>
2023-02-01 17:33:34 +0500

    Added new methods for tests

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -4,14 +4,15 @@ import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Step;
 import org.testng.Assert;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 import static constants.constants.Urls.LOGIN_PAGE_MERCHANT_URL;
 
-public class DashboardPage {
+public class DashboardPage extends BasePage {
     public static final String LOG_OUT_BUTTON_LOCATOR = "//html/body/div[1]/div/div/div[1]/div/div[3]";
     public static final String MERCHANT_ID_LOCATOR = "//*[@class='sc-cOajty jAnegm']/div/div";
-//    public static final String = "";
-//    public static final String = "";
+    public static final String DATE_TABLE_LOCATOR = "//div[4]/div/div/div/div[2]/div/div/div/div/table/tbody/tr/td";
+    public static final String DATE_PICKER_LOCATOR = "#date";
 //    public static final String = "";
 //    public static final String = "";
 //    public static final String = "";
@@ -44,5 +45,11 @@ public class DashboardPage {
         String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
         Assert.assertEquals(currentUrl,LOGIN_PAGE_MERCHANT_URL , "Expected URL is: "
                 + LOGIN_PAGE_MERCHANT_URL + ", actual URL is: " + currentUrl);
+    }
+
+    @Step("Picking date")
+    public void pickDateFromTable(){
+        $(DATE_PICKER_LOCATOR).click();
+        pickRandomItemFromTable(DATE_TABLE_LOCATOR).click();
     }
 }
